2efd23d71f847277b598acda0f3c0b11f949fcb0
Marcos Sanhudo <marcos.sanhudo@gmail.com>
2024-07-15 21:02:16 -0300

    Corrects placement of back-end dependency.

--- node-backend/src/app.js
+++ node-backend/src/app.js
@@ -1,5 +1,16 @@
 import express from "express";
+import connectToDatabase from "./config/mongoDbConnection.js";
 import router from "./routes/index.js";
+
+const connection = await connectToDatabase();
+
+connection.on("error", (error) => {
+    console.error("Connection error", error);
+});
+
+connection.once("open", () => {
+    console.log("Connection successfully set up");
+});
 
 const app = express();
 router(app);
