b1e3ed6b3c63207b8a3dfc5f94f10897ff613484
Marcos Sanhudo <marcos.sanhudo@gmail.com>
2024-08-10 23:58:42 -0300

    Fixes bugs. Improves app architecure.

--- react-native-frontend/businesses/app/BusinessPage.tsx
+++ react-native-frontend/businesses/app/BusinessPage.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { ScrollView, Text, View, StyleSheet, Button } from 'react-native';
+import styles from '@/constants/Styles';
+import { formatTime } from '@/scripts/formatting';
 
-export function BusinessPage({ navigation, route }: any) {
+export const BusinessPage = ({ navigation, route }: any) => {
 
     const BUSINESS_INFO_ENDPOINT = 'http://localhost:3000/businesses/' + route.params.id;
 
@@ -27,7 +29,7 @@ export function BusinessPage({ navigation, route }: any) {
                 <Text style={ styles.h1 }>{ businessInfo.name ? businessInfo.name : "Loading" }</Text>
                 <Text style={ styles.descriptionText }>{ businessInfo.description ? businessInfo.description : "Loading" }</Text>
                 <View style={ styles.box }>
-                    <Text style={ styles.descriptionText }>Open from { businessInfo.openingTime ? businessInfo.openingTime : "Loading" } to { businessInfo.closingTime ? businessInfo.closingTime : "Loading" }</Text>
+                    <Text style={ styles.descriptionText }>Open from { businessInfo.openingTime ? formatTime(businessInfo.openingTime) : "Loading" } to { businessInfo.closingTime ? formatTime(businessInfo.closingTime) : "Loading" }</Text>
                     <View style={ styles.row }>
                         <Text style={ styles.descriptionText }>{ businessInfo.address ? businessInfo.address : "Loading" }</Text>
                         <Button title='Map' />
@@ -44,46 +46,4 @@ export function BusinessPage({ navigation, route }: any) {
         </ScrollView>
     );
 
-}
-
-const styles = StyleSheet.create({
-    page: {
-        backgroundColor: "#FFFFFF",
-        padding: 24,
-        display: 'flex',
-        flexDirection: 'column',
-        gap: 24
-    },
-    h1: {
-        fontSize: 24,
-        fontWeight: 700
-    },
-    h2: {
-        fontSize: 18,
-        fontWeight: 700
-    },
-    descriptionText: {
-        fontSize: 14,
-        fontWeight: 500
-    },
-    descriptionHeading: {
-        fontSize: 14,
-        fontWeight: 700
-    },
-    row: {
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        gap: 16,
-        alignContent: 'center'
-    },
-    box: { 
-        padding: 16,
-        borderColor: "#CCCCCC",
-        borderWidth: 1,
-        borderRadius: 8,
-        display: 'flex',
-        flexDirection: 'column',
-        gap: 16
-    }
-});
+};
--- react-native-frontend/businesses/app/HomePage.tsx
+++ react-native-frontend/businesses/app/HomePage.tsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { ScrollView, StyleSheet, FlatList, View, Text, Pressable } from 'react-native';
+import styles from '@/constants/Styles';
+import { formatTime } from '@/scripts/formatting';
 
 const HomePage = ({ navigation }: any) => {
 
@@ -10,7 +12,7 @@ const HomePage = ({ navigation }: any) => {
     const [businesses, setBusinesses] = useState([
         {
             _id: String,
-            name: "",
+            name: String,
             openingTime: String,
             closingTime: String,
             address: String
@@ -32,7 +34,7 @@ const HomePage = ({ navigation }: any) => {
                 <Text style={ styles.businessName }>{ business.name }</Text>
                 <View
                     style={ styles.businessDescription }>
-                    <Text>Open from { business.openingTime } to { business.closingTime }</Text>
+                    <Text>Open from { formatTime(business.openingTime) } to { formatTime(business.closingTime) }</Text>
                     <Text>{ business.address }</Text>
                 </View>
             </Pressable>);
@@ -59,70 +61,5 @@ const HomePage = ({ navigation }: any) => {
     );
 
 }
-
-const styles = StyleSheet.create({
-    page: {
-        backgroundColor: "#FFFFFF",
-        padding: 24,
-        display: 'flex',
-        flexDirection: 'column',
-        gap: 24
-    },
-    businessListing: {
-        padding: 16,
-        marginRight: 16,
-        backgroundColor: "#EAEAEA",
-        borderWidth: 1,
-        borderColor: "#CCCCCC",
-        borderRadius: 8,
-        width: 240,
-        display: 'flex',
-        flexDirection: 'column',
-        gap: 16
-    },
-    businessName: {
-        fontSize: 18,
-        fontWeight: 700
-    },
-    businessDescription: {
-        display: 'flex',
-        flexDirection: 'column',
-        gap: 8,
-        fontSize: 14,
-        fontWeight: 400
-    },
-    h1: {
-        fontSize: 24,
-        fontWeight: 700
-    },
-    h2: {
-        fontSize: 18,
-        fontWeight: 700
-    },
-    descriptionText: {
-        fontSize: 14,
-        fontWeight: 500
-    },
-    descriptionHeading: {
-        fontSize: 14,
-        fontWeight: 700
-    },
-    row: {
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        gap: 16,
-        alignContent: 'center'
-    },
-    box: { 
-        padding: 16,
-        borderColor: "#CCCCCC",
-        borderWidth: 1,
-        borderRadius: 8,
-        display: 'flex',
-        flexDirection: 'column',
-        gap: 16
-    }
-});
 
 export default HomePage;
