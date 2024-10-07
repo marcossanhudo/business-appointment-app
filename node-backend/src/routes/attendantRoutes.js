b31abcb956ca9c25496c7e9d0f625173c9485356
Marcos Sanhudo <marcos.sanhudo@gmail.com>
2024-10-07 18:41:58 -0300

    Renames GET request functions for consistency.

--- node-backend/src/routes/attendantRoutes.js
+++ node-backend/src/routes/attendantRoutes.js
@@ -3,7 +3,7 @@ import AttendantController from "../controllers/attendantController.js";
 
 const router = new express.Router();
 
-router.get("/attendants", AttendantController.getAllAttendants);
+router.get("/attendants", AttendantController.getAttendants);
 router.get("/attendants/:id", AttendantController.getAttendant);
 router.post("/attendants", AttendantController.createAttendant);
 router.put("/attendants/:id", AttendantController.updateAttendant);
